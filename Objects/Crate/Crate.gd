1371e4b61fe65da630f31464359b47ffe058eb0b
Flipez <code@brauser.io>
2021-09-11 15:37:06 +0200

    update crate

--- Objects/Crate/Crate.gd
+++ Objects/Crate/Crate.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 export var SPEED = 3
+export var never_touched = true
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,16 +9,27 @@ export var SPEED = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  position.x = 192
-  position.y = -8
+  if never_touched:
+    position.x = 192
+    position.y = -8
 
 
 func _process(delta):
+  if never_touched:
+    move_on_belt(delta)
+
+
+func move_on_belt(delta):
   if position.x == 192 && position.y <= 56:
     position.y += SPEED * delta
-  if position.y >= 56 && position.x >= 24:
-    position.y == 56
-    position.x -= SPEED * delta
-  if position.x <= 24 && position.y <= 248:
-    position.x = 24
+  elif position.y == 248 && position.x >= 272:
+    queue_free()
+  elif position.y >= 248 && position.x <= 272:
+    position.y = 248
+    position.x += SPEED * delta
+  elif position.x <= 16 && position.y < 248:
+    position.x = 16
     position.y += SPEED * delta
+  elif position.y >= 56 && position.x > 16:
+    position.y = 56
+    position.x -= SPEED * delta
