eec91b373c809b431e5cad0f4283204d4ae46f8a
skib_ju <juliane.skibbe@dlr.de>
2021-09-17 22:44:00 +0200

    added toilet scene

--- Scenes/Lost.gd
+++ Scenes/Lost.gd
@@ -8,7 +8,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  $Arnold/AnimationPlayer.play("Falling")
+  #$Arnold/AnimationPlayer.play("Falling")
+  $Arnold/AnimationPlayer.play("Toilet")
   pass # Replace with function body.
 
 
