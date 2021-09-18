61ce66245ec668117eda7934e2320103394756c0
Flipez <code@brauser.io>
2021-09-18 13:07:31 +0200

    more fixes

--- Scenes/Keybindings/Keybindings.gd
+++ Scenes/Keybindings/Keybindings.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_Timer_timeout():
     get_tree().change_scene("res://Scenes/Main/MainLevel.tscn")
+
+func _process(delta):
+  if Input.is_action_just_pressed("ui_select"):
+    get_tree().change_scene("res://Scenes/Main/MainLevel.tscn")
